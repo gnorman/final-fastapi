455a71cdf947ee679c672d73d3395d2c43c9ad16
Gregory Norman <gnorman@stateofforms.com>
2024-01-13 16:54:39 -0500

    added :80 to host

--- app/main.py
+++ app/main.py
@@ -26,4 +26,4 @@ app.include_router(vote.router)
       
 @app.get("/")
 def root():
-    return {"message": "Hello World! Deployed my first api."}
+    return {"message": "Hello World! Deployed my first api!"}
